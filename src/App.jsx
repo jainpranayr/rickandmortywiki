c3b934a0667988b05c24120b029e9ff173a67a4b
Pranay Jain <jpranay688@gmail.com>
2021-12-25 18:51:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='text-center text-primary my-4  ubuntu'>Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
